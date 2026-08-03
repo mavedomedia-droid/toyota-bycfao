4ed3d885b8d9da8bb1fb966aa67020b2f8f47d1e
mavedomedia-droid <mavedomedia@gmail.com>
2026-08-03 21:06:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Single place for your Google Apps Script web‑app URL.
 // All landing pages (quiz, thank‑you, callback, separate forms)
 // reference this file so you only change the URL once.
-var GAS_URL = "https://script.google.com/macros/s/AKfycbw_JZCf3loQkpykdGRn-qDPOBnByNyA8CxtEzaQG6AKAheCkTgJKeEPkWSCf4XLbWAd0Q/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbwFnmeOK0g6j5Mulp97VLdvyLIzZ_CH1AnFtTDJy5tatn4-b-9zEkcU_kX6nBkXtyD5wQ/exec";
